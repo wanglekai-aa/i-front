6f71ca8532022b009620af97e3b723de6886bf75
wanglekai <xiaoguailele@foxmail.com>
2022-07-24 17:06:03 +0800

    vite 通用组件自动化注册

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,9 +1,19 @@
-import svgIcon from './svg-icon/SvgIcon.vue'
-import popUp from './popup/PopUp.vue'
+import { defineAsyncComponent } from 'vue'
 
 export default {
   install(app) {
-    app.component('m-svg-icon', svgIcon)
-    app.component('m-popup', popUp)
+    // vite 通用组件自动化注册
+
+    // 获取当前路径下所有文件夹中的 .vue 文件
+    const components = import.meta.glob('./*/*.vue')
+    // 遍历获取到的组件模块
+    // console.log(components)
+    for (const [key, value] of Object.entries(components)) {
+      // 拼接组件注册的 name
+      const componentName = 'm-' + key.replace('./', '').split('/')[0]
+
+      // 通过 defineAsyncComponent 异步导入指定路径下的组件
+      app.component(componentName, defineAsyncComponent(value))
+    }
   }
 }
