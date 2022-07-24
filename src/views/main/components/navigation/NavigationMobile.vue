91963534de79924e8ff6c74e6c7ec29b04d81e66
wanglekai <xiaoguailele@foxmail.com>
2022-07-24 10:32:18 +0800

    移动端 slider 样式, 滑块切换时逻辑与滚动动画

--- src/views/main/components/navigation/NavigationMobile.vue
+++ src/views/main/components/navigation/NavigationMobile.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="bg-white sticky top-0 left-0 z-10">
-    <ul class="relative flex overflow-x-auto p-1 text-xs text-zinc-600">
+    <ul
+      ref="ulTarget"
+      class="relative flex overflow-x-auto p-1 text-xs text-zinc-600"
+    >
+      <!-- 滑块 -->
+      <li
+        ref="sliderTarget"
+        :style="sliderStyle"
+        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200"
+      ></li>
       <!-- 汉堡按钮 -->
       <li
         class="fixed z-20 top-0 right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white"
@@ -9,8 +18,11 @@
       </li>
       <li
         class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
-        v-for="category in categories"
+        :class="{ 'text-zinc-100': currentCategoryIndex === index }"
+        v-for="(category, index) in categories"
         :key="category.id"
+        :ref="setItemRef"
+        @click="handleTagClick(index)"
       >
         {{ category.name }}
       </li>
@@ -19,10 +31,52 @@
 </template>
 
 <script setup>
+import { ref } from '@vue/reactivity'
+import { onBeforeUpdate, watch } from '@vue/runtime-core'
+import { useScroll } from '@vueuse/core'
+
 defineProps({
   categories: {
     type: Array,
     required: true
   }
 })
+
+// 滑块样式
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '60px'
+})
+
+// 获取所有 item 元素
+let itemRefs = []
+const setItemRef = (el) => {
+  if (el) itemRefs.push(el)
+}
+// 数据改变之后， DOM 变化之前
+onBeforeUpdate(() => {
+  itemRefs = []
+})
+
+// 选中 item 下标
+const currentCategoryIndex = ref(0)
+
+watch(currentCategoryIndex, (val) => {
+  // 选中元素的 left 和 width
+  const { left, width } = itemRefs[val].getBoundingClientRect()
+  sliderStyle.value = {
+    transform: `translateX(${ulScrollLeft.value + left - 10}px)`,
+    width: width + 'px'
+  }
+})
+
+const ulTarget = ref(null)
+// 通过 vueuse - useScroll 获取响应式 scroll 滚动距离
+let { x: ulScrollLeft } = useScroll(ulTarget)
+
+// 切换事件
+const handleTagClick = (index) => {
+  if (currentCategoryIndex.value === index) return
+  currentCategoryIndex.value = index
+}
 </script>
