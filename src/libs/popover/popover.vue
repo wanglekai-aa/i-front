2f54245928f0615a308b11d72b45cf1c201f6eac
wanglekai <xiaoguailele@foxmail.com>
2022-07-26 01:50:02 +0800

    通用组件：popover 气泡卡片基础功能实现

---
+++ src/libs/popover/popover.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="relative" @mouseenter="onMouseenter" @mouseleave="onMouseleave">
+    <div>
+      <slot name="reference" />
+    </div>
+
+    <!-- 气泡展示动画 -->
+    <transition name="slide">
+      <div
+        v-show="isVisable"
+        class="absolute p-1 z-20 bg-white border rounded-md"
+      >
+        <slot />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script setup>
+import { ref } from '@vue/reactivity'
+
+const isVisable = ref(false)
+
+// 鼠标移入
+const onMouseenter = () => {
+  isVisable.value = true
+}
+
+// 鼠标移出
+const onMouseleave = () => {
+  isVisable.value = false
+}
+</script>
+
+<style lang="scss" scoped>
+.slide-enter-active {
+  transition: opacity 0.3s, transform 0.3s;
+}
+
+.slide-leave-active {
+  transition: opacity 0.3s, transform 0.3s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateY(20px);
+  opacity: 0;
+}
+</style>
--- src/views/layout/components/header/header-theme.vue
+++ src/views/layout/components/header/header-theme.vue
@@ -1,3 +1,35 @@
 <template>
-  <div>theme</div>
+  <m-popover>
+    <template #reference>
+      <m-svg-icon
+        name="theme-light"
+        class="guide-theme w-4 h-4 p-1 cursor-pointer rounded-sm duration-200 outline-none hover:bg-zinc-100/60"
+        fillClass="fill-zinc-900"
+      />
+    </template>
+
+    <div class="overflow-hidden w-[140px]">
+      <div
+        class="flex items-center p-1 cursor-pointer rounded hover:bg-zinc-100/60"
+        v-for="item in themeArr"
+        :key="item.id"
+      >
+        <m-svg-icon
+          :name="item.icon"
+          class="w-1.5 h-1.5 mr-1"
+          fillClass="fill-zinc-900"
+        />
+        <span class="text-zinc-800 text-sm" v-text="item.name"></span>
+      </div>
+    </div>
+  </m-popover>
 </template>
+
+<script setup>
+import { THEME_DARK, THEME_LIGHT, THEME_SYSTEM } from '@/constants'
+const themeArr = [
+  { id: '101', type: THEME_LIGHT, icon: 'theme-light', name: '极简白' },
+  { id: '102', type: THEME_DARK, icon: 'theme-dark', name: '极夜黑' },
+  { id: '103', type: THEME_SYSTEM, icon: 'theme-system', name: '跟随系统' }
+]
+</script>
