6b433d6ea93cf696dcc1e85bc032bbce733d327e
wanglekai <xiaoguailele@foxmail.com>
2022-07-25 14:04:43 +0800

    PC 端 Header 模块

---
+++ src/views/layout/components/header/header-search/headerSearch.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>header-search</div>
+</template>
--- src/views/layout/components/header/header.vue
+++ src/views/layout/components/header/header.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>Header</div>
+  <dir class="w-full bg-white border-b border-b-zinc-200 py-1 m-0">
+    <div class="flex items-center">
+      <router-link to="/" class="w-4 h-4 mr-2">
+        <img src="@/assets/logo.jpg" class="w-full" alt="logo" />
+      </router-link>
+      <header-search class="mr-1" />
+      <header-theme class="mr-1" />
+      <header-my />
+    </div>
+  </dir>
 </template>
+
+<script setup>
+import headerSearch from './header-search/headerSearch.vue'
+import headerMy from './header-my.vue'
+import headerTheme from './header-theme.vue'
+</script>
