d0030d695e10a846e8fb7a1cbe3c27b16e40aa5e
wanglekai <xiaoguailele@foxmail.com>
2022-07-25 13:23:18 +0800

    处理 PC 端基础架构

---
+++ src/views/layout/LayoutVIew.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-screen">
+    <header-vue class="h-header" />
+    <main-vue class="h-main" />
+    <floating-vue />
+  </div>
+</template>
+
+<script setup>
+import headerVue from './components/header/header.vue'
+import floatingVue from './components/floating.vue'
+import mainVue from './components/main.vue'
+</script>
