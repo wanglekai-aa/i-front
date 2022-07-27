ddb8ff7e0e33447590fa06d8cf0a8ef7ec1ccba6
wanglekai <xiaoguailele@foxmail.com>
2022-07-28 00:45:20 +0800

    Theme: 跟随系统的主题变更

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,23 +1,35 @@
 import store from '@/store'
 import { watch } from 'vue'
-import { THEME_DARK, THEME_LIGHT } from '@/constants'
+import { THEME_DARK, THEME_LIGHT, THEME_SYSTEM } from '@/constants'
+
+// 监听系统主题变更
+let matchMedia
+const watchSystemThemeChange = () => {
+  if (matchMedia) return
+  matchMedia = window.matchMedia('(prefers-color-scheme: dark)')
+  matchMedia.onchange = () => {
+    changeTheme(THEME_SYSTEM)
+  }
+}
+
+const changeTheme = (theme) => {
+  let themeClass = ''
+  switch (theme) {
+    case THEME_DARK:
+      themeClass = 'dark'
+      break
+    case THEME_LIGHT:
+      themeClass = 'light'
+      break
+    case THEME_SYSTEM:
+      watchSystemThemeChange()
+      themeClass = matchMedia.matches ? 'dark' : 'light'
+      break
+  }
+  document.querySelector('html').className = themeClass
+}
 
 // 初始化主题
 export default () => {
-  watch(
-    () => store.getters.themeType,
-    (val) => {
-      let themeClass = ''
-      switch (val) {
-        case THEME_DARK:
-          themeClass = 'dark'
-          break
-        case THEME_LIGHT:
-          themeClass = 'light'
-          break
-      }
-      document.querySelector('html').className = themeClass
-    },
-    { immediate: true }
-  )
+  watch(() => store.getters.themeType, changeTheme, { immediate: true })
 }
