8ee3a9b85e6be08475f5585288d9e49260e2f826
wanglekai <xiaoguailele@foxmail.com>
2022-07-27 19:05:29 +0800

    PC 端 navigationBar 私有逻辑处理

--- src/views/main/components/navigation/NavigationPc.vue
+++ src/views/main/components/navigation/NavigationPc.vue
@@ -2,21 +2,47 @@
   <div class="bg-white sticky top-0 left-0 w-full z-10">
     <ul
       class="w-[800px] relative flex flex-wrap justify-center overflow-x-auto px-[10px] py-1 text-xs text-zinc-600 duration-300 overflow-hidden mx-auto"
+      :class="[isOpenCategory ? 'h-[206px]' : 'h-[56px]']"
     >
       <!-- 下展开箭头 -->
       <div
         class="absolute right-1 bottom-1 z-20 p-1 cursor-pointer duration-200 hover:bg-zinc-200"
+        @click="triggerState"
       >
-        <m-svg-icon name="unfold" class="w-1 h-1" fillClass="fill-zinc-900" />
+        <m-svg-icon
+          :name="isOpenCategory ? 'fold' : 'unfold'"
+          class="w-1 h-1"
+          fillClass="fill-zinc-900"
+        />
       </div>
       <!-- category item  -->
       <li
-        v-for="item in $store.getters.categories"
+        v-for="(item, index) in $store.getters.categories"
         :key="item.id"
         class="shrink-0 px-1.5 py-0 z-10 duration-200 last:mr-4 text-zinc-900 text-base font-bold h-4 leading-4 cursor-pointer hover:bg-zinc-200 rounded mr-1 mb-1"
+        :class="{
+          'text-zinc-900 bg-zinc-200': currentCategoryIndex === index
+        }"
+        @click="onItemClick(index)"
       >
         {{ item.name }}
       </li>
     </ul>
   </div>
 </template>
+
+<script setup>
+import { ref } from '@vue/reactivity'
+
+// 状态切换
+const isOpenCategory = ref(false)
+const triggerState = () => {
+  isOpenCategory.value = !isOpenCategory.value
+}
+
+// 选中状态
+const currentCategoryIndex = ref(0)
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
+</script>
