66e110bb865ae416f5070d7f4a93a46538daf599
wanglekai <xiaoguailele@foxmail.com>
2022-07-30 01:38:42 +0800

    深入 vite：指令的自动注册

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,7 +1,15 @@
-import lazy from './modules/lazy'
-
+// 全局指令注册
 export default {
-  install(app) {
-    app.directive('lazy', lazy)
+  async install(app) {
+    // https://cn.vitejs.dev/guide/features.html#glob-import
+    // import.meta.globEager 为同步导入
+    const directives = import.meta.globEager('./modules/*.js')
+    for (const [key, value] of Object.entries(directives)) {
+      // 拼接组件注册的 name
+      const arr = key.split('/')
+      const directiveName = arr[arr.length - 1].replace('.js', '')
+      // 完成注册
+      app.directive(directiveName, value.default)
+    }
   }
 }
