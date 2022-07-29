82db876e3eef600c153ecde4789c05e3a93ee0d6
wanglekai <xiaoguailele@foxmail.com>
2022-07-30 02:18:45 +0800

    图片懒加载：指定彩色占位图

--- src/views/main/components/list/listItem.vue
+++ src/views/main/components/list/listItem.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="bg-white dark:bg-zinc-900 xl:dark:bg-zinc-800 rounded pb-1">
-    <div class="relative w-full rounded cursor-zoom-in group">
+    <div
+      class="relative w-full rounded cursor-zoom-in group"
+      :style="{
+        backgroundColor: randomRGB()
+      }"
+    >
       <img
         v-lazy
         class="w-full rounded bg-transparent"
@@ -54,6 +59,7 @@
 </template>
 
 <script setup>
+import { randomRGB } from '@/utils/color.js'
 defineProps({
   data: {
     required: true,
