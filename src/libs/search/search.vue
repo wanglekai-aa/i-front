797210bfbb55abe64c1d2d3acc49d5aa6f540d92
Wanglekai <xiaoguailele@foxmail.com>
2022-07-31 16:50:42 +0800

    样式修正： scrollBar, 自定义滚动条样式

--- src/libs/search/search.vue
+++ src/libs/search/search.vue
@@ -44,7 +44,7 @@
       <div
         v-if="$slots.dropdown"
         v-show="isFocus"
-        class="max-h-[368px] w-full text-base overflow-auto bg-white absolute z-20 left-0 top-[56px] p-2 rounded border border-zinc-200 duration-200 hover:shadow-3xl dark:bg-zinc-800 dark:border-zinc-600"
+        class="max-h-[368px] w-full text-base overflow-auto bg-white absolute z-20 left-0 top-[56px] p-2 rounded border border-zinc-200 duration-200 hover:shadow-3xl dark:bg-zinc-800 dark:border-zinc-600 scrollbar-thin scrollbar-thumb-zinc-200 dark:scrollbar-thumb-zinc-900 scrollbar-track-transparent"
       >
         <slot name="dropdown" />
       </div>
