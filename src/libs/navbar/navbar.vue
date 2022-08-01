1b998912201c6056fd1347000be500a731cddd28
Wanglekai <xiaoguailele@foxmail.com>
2022-08-01 17:49:47 +0800

    通用组件：构建 navbar

---
+++ src/libs/navbar/navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    class="w-full h-5 border-b flex items-center z-10 bg-white dark:bg-zinc-800 border-zinc-200 dark:border-b-zinc-700"
+    :class="[sticky ? 'sticky top-0 left-0' : 'relative']"
+  >
+    <!-- 左 -->
+    <div
+      class="h-full w-5 absolute left-0 flex items-center justify-center"
+      @click="onClickLeft"
+    >
+      <slot name="left">
+        <m-svg-icon
+          name="back"
+          class="w-2 h-2"
+          fillClass="fill-zinc-900 dark:fill-zinc-200"
+        />
+      </slot>
+    </div>
+    <!-- 中 -->
+    <div
+      class="h-full flex items-center justify-center m-auto font-bold text-base text-zinc-900 dark:text-zinc-200"
+    >
+      <slot />
+    </div>
+    <!-- 右 -->
+    <div
+      class="h-full w-5 absolute right-0 flex items-center justify-center"
+      @click="onClickRight"
+    >
+      <slot name="right" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const props = defineProps({
+  clickLeft: Function,
+  clickRight: Function,
+  sticky: Boolean
+})
+
+const router = useRouter()
+const onClickLeft = () => {
+  if (props.clickLeft) {
+    return props.clickLeft()
+  }
+  router.back()
+}
+const onClickRight = () => {
+  props.clickRight && props.clickRight()
+}
+</script>
