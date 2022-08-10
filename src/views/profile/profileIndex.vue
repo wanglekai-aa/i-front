b0e288f63f57d039a340284af309a79a0edc799c
Wanglekai <xiaoguailele@foxmail.com>
2022-08-11 03:27:14 +0800

    处理不保存时的同步问题

--- src/views/profile/profileIndex.vue
+++ src/views/profile/profileIndex.vue
@@ -63,8 +63,7 @@
             >用户名</span
           >
           <m-input
-            :modelValue="$store.getters.userInfo.nickname"
-            @update:modelValue="changeStoreUserInfo('nickname', $event)"
+            v-model="userInfo.nickname"
             class="w-full"
             type="text"
             max="20"
@@ -76,8 +75,7 @@
             >职位</span
           >
           <m-input
-            :modelValue="$store.getters.userInfo.title"
-            @update:modelValue="changeStoreUserInfo('title', $event)"
+            v-model="userInfo.title"
             class="w-full"
             type="text"
           ></m-input>
@@ -88,8 +86,7 @@
             >公司</span
           >
           <m-input
-            :modelValue="$store.getters.userInfo.company"
-            @update:modelValue="changeStoreUserInfo('company', $event)"
+            v-model="userInfo.company"
             class="w-full"
             type="text"
           ></m-input>
@@ -100,8 +97,7 @@
             >个人主页</span
           >
           <m-input
-            :modelValue="$store.getters.userInfo.homePage"
-            @update:modelValue="changeStoreUserInfo('homePage', $event)"
+            v-model="userInfo.homePage"
             class="w-full"
             type="text"
           ></m-input>
@@ -112,8 +108,7 @@
             >个人介绍</span
           >
           <m-input
-            :modelValue="$store.getters.userInfo.introduction"
-            @update:modelValue="changeStoreUserInfo('introduction', $event)"
+            v-model="userInfo.introduction"
             class="w-full"
             type="textarea"
             max="50"
@@ -187,19 +182,14 @@ const onLogoutClick = () => {
   })
 }
 
-// 数据本地双向同步
-const changeStoreUserInfo = (key, value) => {
-  store.commit('user/setUserInfo', {
-    ...store.getters.userInfo,
-    [key]: value
-  })
-}
+const userInfo = ref(store.getters.userInfo)
 
 // 保存修改信息
 const loading = ref(false)
 const onChangeProfile = async () => {
   loading.value = true
   await putProfile(store.getters.userInfo)
+  store.commit('user/setUserInfo', userInfo.value)
   mMessage('success', '修改成功～')
   loading.value = false
 }
