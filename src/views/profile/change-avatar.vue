9b05fc82f3162c45d4e9ac686974aa0da7d72e26
Wanglekai <xiaoguailele@foxmail.com>
2022-08-12 00:08:04 +0800

    头像裁剪构建方案

--- src/views/profile/change-avatar.vue
+++ src/views/profile/change-avatar.vue
@@ -10,7 +10,10 @@
 
     <img class="" ref="imageTarget" :src="blob" />
 
-    <m-button class="mt-4 w-[80%] xl:w-1/2" @click="onConfirmClick"
+    <m-button
+      class="mt-4 w-[80%] xl:w-1/2"
+      @click="onConfirmClick"
+      :loading="loading"
       >确定</m-button
     >
   </div>
@@ -18,10 +21,33 @@
 
 <script>
 const EMITS_CLOSE = 'close'
+
+// 移动端配置对象
+const mobileOptions = {
+  // 将裁剪框限制在画布的大小
+  viewMode: 1,
+  // 移动画布，裁剪框不动
+  dragMode: 'move',
+  // 裁剪框固定纵横比：1:1
+  aspectRatio: 1,
+  // 裁剪框不可移动
+  cropBoxMovable: false,
+  // 不可调整裁剪框大小
+  cropBoxResizable: false
+}
+
+// PC 端配置对象
+const pcOptions = {
+  // 裁剪框固定纵横比：1:1
+  aspectRatio: 1
+}
 </script>
 
 <script setup>
 import { isMobileTerminal } from '@/utils/flexible'
+import Cropper from 'cropperjs'
+import 'cropperjs/dist/cropper.css'
+import { ref, onMounted } from 'vue'
 
 defineProps({
   blob: {
@@ -32,10 +58,17 @@ defineProps({
 
 const emits = defineEmits([EMITS_CLOSE])
 
-/**
- * 确定按钮点击事件
- */
-const onConfirmClick = () => {}
+const loading = ref(false)
+// 确定按钮点击事件
+const onConfirmClick = () => {
+  // 开启 loading
+  loading.value = true
+  // 获取裁剪后的图片
+  cropper.getCroppedCanvas().toBlob((blob) => {
+    // 裁剪后的 blob 地址
+    console.log(URL.createObjectURL(blob))
+  })
+}
 
 /**
  * 关闭事件
@@ -43,4 +76,19 @@ const onConfirmClick = () => {}
 const close = () => {
   emits(EMITS_CLOSE)
 }
+
+// 图片裁剪处理
+const imageTarget = ref(null)
+let cropper = null
+onMounted(() => {
+  /**
+   * 接收两个参数：
+   * 1. 需要裁剪的图片 DOM
+   * 2. options 配置对象
+   */
+  cropper = new Cropper(
+    imageTarget.value,
+    isMobileTerminal.value ? mobileOptions : pcOptions
+  )
+})
 </script>
